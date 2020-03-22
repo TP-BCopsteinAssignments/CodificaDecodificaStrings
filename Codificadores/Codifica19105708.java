e146b3e4725275cf96608d25e630de2e2745124f
joaokanta <jaonunes16@gmail.com>
2020-03-22 17:10:09 -0300

    Classe base

---
+++ Codificadores/Codifica19105708.java
@@ -0,0 +1,23 @@
+package Codificadores;
+
+public class Codifica19105708 implements Codifica {
+
+    public String getNomeAutor() {
+        return "João Vítor Lioti Nunes";
+    }
+
+    public String getMatriculaAutor(){
+        return "19105708";
+    }
+
+    public String codifica(String s){
+        return null;
+    }
+
+    public String decodifica(String s){
+        return null;
+    }
+
+
+
+}
