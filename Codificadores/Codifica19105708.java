497e91c28611a84cf58058a307703fa67b2421c6
joaokanta <jaonunes16@gmail.com>
2020-03-22 17:39:27 -0300

    Metodos implementados

--- Codificadores/Codifica19105708.java
+++ Codificadores/Codifica19105708.java
@@ -1,23 +1,51 @@
 package Codificadores;
 
 public class Codifica19105708 implements Codifica {
+    private static String tabela = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz@#";
+    private static int deslc = 1;
 
+    @Override
     public String getNomeAutor() {
         return "João Vítor Lioti Nunes";
     }
 
+    @Override
     public String getMatriculaAutor(){
         return "19105708";
     }
 
-    public String codifica(String s){
-        return null;
+    @Override
+    public String codifica(String str){
+        String codificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                codificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        codificada += tabela.charAt(i + deslc);
+                    }
+                }
+            }
+        }
+        return codificada;
+
     }
 
-    public String decodifica(String s){
-        return null;
+    @Override
+    public String decodifica(String str){
+        String decodificada = "";
+        for (char s : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(s)) {
+                decodificada += s;
+            } else {
+                for (int i = 0; i < tabela.length(); i++) {
+                    if (tabela.charAt(i) == s) {
+                        decodificada += tabela.charAt(i - deslc);
+                    }
+                }
+            }
+        }
+        return decodificada;
     }
-
-
-
 }
