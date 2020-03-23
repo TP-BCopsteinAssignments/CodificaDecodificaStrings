141c8905d4027f0f51095d87f11133809e09c408
Ncjhsn <niceniick@gmail.com>
2020-03-22 23:16:43 -0300

    bug fix

--- Codificadores/Codifica19102850.java
+++ Codificadores/Codifica19102850.java
@@ -1,50 +1,52 @@
 package Codificadores;
 
-public class Codifica19108146 implements Codifica {
-  private static String tabela = "pK5HSwPdEVmOv3aTjINg7McrFYoA8DLzhxG9XqlsRe1nJ2tubW0Bfky6Qi4CUZ#@)";
-  private static int desloc = 3;
-  
-  @Override
-  public String getNomeAutor() {
-    return "Vinicius de Borba";
-  }
+public class Codifica19102850 implements Codifica {
+    private static String tabela = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz@#";
+    private static int deslc = 3;
 
-  @Override
-  public String getMatriculaAutor() {
-    return "19108146";
-  }
+    @Override
+    public String getMatriculaAutor() {
+        return "19102850";
+    }
 
-  @Override
-  public String codifica(String str){
-    String codificada = "";
+    @Override
+    public String getNomeAutor() {
+        return "Nicolas Pietro";
+    }
+
+    @Override
+    public String codifica(String str) {
+        String codificada = "";
         for (char s : str.toCharArray()) {
             if (!Character.isLetterOrDigit(s)) {
                 codificada += s;
             } else {
                 for (int i = 0; i < tabela.length(); i++) {
                     if (tabela.charAt(i) == s) {
-                        codificada += tabela.charAt(i + desloc);
+                        codificada += tabela.charAt(i + deslc);
                     }
                 }
             }
         }
         return codificada;
-  }
+    }
 
-  @Override
-  public String decodifica(String str){
-    String decodificada = "";
+    @Override
+    public String decodifica(String str) {
+        String decodificada = "";
         for (char s : str.toCharArray()) {
             if (!Character.isLetterOrDigit(s)) {
                 decodificada += s;
             } else {
                 for (int i = 0; i < tabela.length(); i++) {
                     if (tabela.charAt(i) == s) {
-                        decodificada += tabela.charAt(i - desloc);
+                        decodificada += tabela.charAt(i - deslc);
                     }
                 }
             }
         }
         return decodificada;
-  }
+    }
+
+
 }
