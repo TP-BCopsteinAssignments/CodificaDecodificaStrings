405adb3cc6210a90de926fec4acf534bd5f656f7
Igor Dourado <igor.d@edu.pucrs.br>
2020-08-26 19:49:07 -0300

    commit decoder

--- Codificadores/Codifica19204004.java
+++ Codificadores/Codifica19204004.java
@@ -4,12 +4,12 @@ public class Codifica19204004 implements Codifica{
     
     @Override
     public String codifica(String str){
-        return "Palmeiras não tem mundial"+ str.toUpperCase() + "Palmeiras não tem mundial";
+        return "Palmeiras nao tem mundial e "+ str.toUpperCase() + " e o Palmeiras nao tem mundial";
     }
 
     @Override
     public String decodifica(String str){
-        return str;
+        return "Palmeiras nao tem mundial e "+ str.toUpperCase() + " e o Palmeiras nao tem mundial";
     }
 
     @Override
