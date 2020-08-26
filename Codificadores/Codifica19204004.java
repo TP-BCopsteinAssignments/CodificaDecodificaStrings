115c1fbe50bf50e77740b39a83a06262ba448c0b
Igor Dourado <igor.d@edu.pucrs.br>
2020-08-26 20:27:25 -0300

    Commit com tudo certo

--- Codificadores/Codifica19204004.java
+++ Codificadores/Codifica19204004.java
@@ -4,12 +4,24 @@ public class Codifica19204004 implements Codifica{
     
     @Override
     public String codifica(String str){
-        String aux = "Palmeiras nao tem mundial e "+ str.toUpperCase() + " e o Palmeiras nao tem mundial";
+        String aux = "Palmeiras nao tem mundial e "+ str.toUpperCase();
         return aux;
     }
 
     @Override
     public String decodifica(String str){
+        String vetor[] = str.split(" ");
+        String aux = "";
+        for(int i=0; i<vetor.length;i++){
+            if(i==vetor.length-1){
+                aux += ".";
+            }
+            else if(i>4){
+                aux += vetor[i] + " ";
+            }
+        }
+        aux = aux.toLowerCase();
+        aux = aux.substring(0,1).toUpperCase().concat(aux.substring(1));
         return str;
     }
 
