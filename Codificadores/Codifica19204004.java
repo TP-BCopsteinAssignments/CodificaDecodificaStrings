9e6eff70a1a803a08a34e8983662fc2732035c00
Igor Dourado <igor.d@edu.pucrs.br>
2020-08-26 19:46:38 -0300

    Commit sem erro

--- Codificadores/Codifica19204004.java
+++ Codificadores/Codifica19204004.java
@@ -2,43 +2,14 @@ package Codificadores;
 
 public class Codifica19204004 implements Codifica{
     
-    private static String tabela = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
-    private static int deslc = 4;
-
     @Override
-    public String codifica(String str) {
-        String codificada = "";
-        for (char s : str.toCharArray()) {
-            if (!Character.isLetterOrDigit(s)) {
-                codificada += s;
-            } else {
-                for (int i = 0; i < tabela.length(); i++) {
-                    if (tabela.charAt(i) == s) {
-                        int pos = Math.floorMod((i+deslc), tabela.length());
-                        codificada += tabela.charAt(pos);
-                    }
-                }
-            }
-        }
-        return codificada;
+    public String codifica(String str){
+        return "Palmeiras não tem mundial"+ str.toUpperCase() + "Palmeiras não tem mundial";
     }
 
     @Override
-    public String decodifica(String str) {
-        String decodificada = "";
-        for (char s : str.toCharArray()) {
-            if (!Character.isLetterOrDigit(s)) {
-                decodificada += s;
-            } else {
-                for (int i = 0; i < tabela.length(); i++) {
-                    if (tabela.charAt(i) == s) {
-                        int pos = Math.floorMod((i-deslc), tabela.length());
-                        decodificada += tabela.charAt(pos);
-                    }
-                }
-            }
-        }
-        return decodificada;
+    public String decodifica(String str){
+        return str;
     }
 
     @Override
